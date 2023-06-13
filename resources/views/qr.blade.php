755bf0da2edc31e8b25eded7abdb74a0387de59d
DaniBalsera <daniseneca.df@gmail.com>
2023-06-13 16:32:35 +0200

    Nuevos cambios

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -2,21 +2,50 @@
 
 @section('title', 'Dashboard')
 
-{{-- @section('plugins.FullCalendar', true);
-@section('plugins.Sweetalert2', true);
-@section('plugins.Datatables', true); --}}
-
 @section('content_header')
-    <h1>Qr</h1>
-@stop
-@section('css')
 
+@stop
 
 @section('content')
-<link rel="stylesheet" href="/css/admin_custom.css">
-    <div class="card-body">
 
-  
-        {!! QrCode::size(400)->generate($datos) !!}
+<link rel="stylesheet" href="/css/admin_custom.css">
+<style>
+    .content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #DCDAD9 ;
+    }
+
+
+.qr-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    margin-bottom: 100px;
+}
+
+.qr-title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+
+.qr-image {
+    margin-bottom: 20px;
+}
+</style>
+<div class="content">
+    <div class="qr-container">
+        <h2 class="qr-title">STUDENT QR</h2>
+        <div class="qr-image">
+            {!! QrCode::size(400)->generate($datos) !!}
+        </div>
     </div>
+</div>
 @stop
